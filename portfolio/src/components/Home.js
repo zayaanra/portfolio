8154a704cd8ab54122d6a627680408381d1c83ee
Zayaan <zayaanra@buffalo.edu>
2022-12-17 15:50:02 -0500

    Added image for display

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import me from '../images/me.jpg'
 
 const styles = {
     container: {
@@ -17,13 +18,24 @@ const styles = {
         borderRadius: 100,
         padding: 20
     },
+    image: {
+        borderRadius: 500,
+        borderColor: "white",
+        width: 200,
+        padding: 50,
+    }
 }
 
 export default function Home() {
     return (
-        <section style={styles.container}>
-            <button style={styles.button} type="button" >contact</button>
-            <button style={styles.button} type="button">projects</button>
+        <section>
+            <section style={styles.container}>
+                <button style={styles.button} type="button" >contact</button>
+                <button style={styles.button} type="button">projects</button>
+            </section>
+            <section style={styles.container}>
+                <img style={styles.image} src={me}></img>
+            </section>
         </section>
     )
 }
