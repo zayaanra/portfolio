50e0056963357367f0643870032ee3ba67133853
Zayaan <zayaanra@buffalo.edu>
2022-12-17 14:55:02 -0500

    Added a home component

---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,28 @@
+const styles = {
+    container: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+        
+    },
+    header: {
+        fontFamily: "Mukta-Regular",
+        fontSize: 20,
+        color: 'white'
+    }
+}
+
+//const button = 
+
+export default function Home() {
+    return (
+        <section style={styles.container}>
+            <div style={styles.header}>
+                <p>
+                    contact projects
+                </p>
+                <p>sd</p>
+            </div>
+        </section>
+    )
+}
